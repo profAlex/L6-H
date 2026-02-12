67d058d41f319fc69a26c8283534132534797676
Alex <steblyakov.aleksandr@gmail.com>
2026-02-13 00:14:06 +0300

    After the code review

--- dest/repository/collection-names.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CollectionNames = void 0;
-var CollectionNames;
-(function (CollectionNames) {
-    CollectionNames["Posts"] = "postsCollection";
-    CollectionNames["Blogs"] = "bloggersCollection";
-})(CollectionNames || (exports.CollectionNames = CollectionNames = {}));
--- dest/routers/router-pathes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BLOGS_PATH = exports.POSTS_PATH = exports.TESTING_PATH = void 0;
-exports.TESTING_PATH = '/testing';
-exports.POSTS_PATH = '/posts';
-exports.BLOGS_PATH = '/blogs';
